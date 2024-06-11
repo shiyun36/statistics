a0abe3ce224b3dfe99ee6575c66faa2a3b9bf56b
shiyun36 <111891177+shiyun36@users.noreply.github.com>
2024-06-11 14:39:13 +0200

    Update demo_statisticalAnalysis.py

--- demo_statisticalAnalysis.py
+++ demo_statisticalAnalysis.py
@@ -2,3 +2,19 @@
 !pip install scipy
 !pip install python-docx
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from tabulate import tabulate
+from scipy.stats import *
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from statsmodels.multivariate.manova import MANOVA
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelEncoder
+
+
